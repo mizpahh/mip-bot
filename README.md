bc5286d8e4d459ea6007c28692ebf69de044b7ac
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-11-30 02:15:12 -0800

    Add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mip bot
+
+https://discord.com/oauth2/authorize?client_id=915106432569974824&scope=bot&permissions=3072
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+use serenity::{
+    async_trait,
+    client::{Context, EventHandler},
+    model::channel::Message,
+    Client,
+};
+
+static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i:\b(?:i'm|im)\s+)([\s\S]+)").unwrap());
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.id == ctx.cache.current_user_id().await {
+            return;
+        }
+        if let Some(caps) = RE.captures(&msg.content) {
+            if let Some(mat) = caps.get(1) {
+                let _ = msg
+                    .channel_id
+                    .say(
+                        ctx.http,
+                        format!("Hi {}, nice to meet you, I'm mip!", mat.as_str()),
+                    )
+                    .await;
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = Client::builder(std::env!("DISCORD_TOKEN"))
+        .event_handler(Handler)
+        .await?;
+    client.start().await?;
+    Ok(())
+}
